161a1f8132aeb42f2647d139bacd3ee1246d960e
OsCordero <oscarcordero97@gmail.com>
2022-01-28 21:57:55 -0600

    adding query test

--- src/apollo-client/queries/index.ts
+++ src/apollo-client/queries/index.ts
@@ -58,3 +58,37 @@ export const userPairsQuery = gql`
   }
   ${pairFieldsQuery}
 `;
+
+export const pairQ = gql`
+  {
+    pair(id: "0xa389f9430876455c36478deea9769b7ca4e3ddb1") {
+      id
+      token0 {
+        symbol
+      }
+      token1Price
+      token1 {
+        symbol
+      }
+      dayData {
+        volumeUSD
+      }
+      hourData(
+        first: 24
+        where: { date_gt: 1622419200 }
+        orderBy: date
+        orderDirection: desc
+      ) {
+        date
+        untrackedVolumeUSD
+        volumeToken0
+        volumeToken1
+        volumeUSD
+      }
+      reserve0
+      reserve1
+      reserveAVAX
+      reserveUSD
+    }
+  }
+`;
